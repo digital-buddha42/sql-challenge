11a7ba1a492345f5f41f24f2f7129c3b1fd87000
Alec Banerji <alec.banerji@gmail.com>
2023-08-12 01:20:49 -0700

    Finished?

--- EmployeeSQL/company_analysis_queries.sql
+++ EmployeeSQL/company_analysis_queries.sql
@@ -49,7 +49,34 @@ WHERE e.first_name = 'Hercules'
 AND e.last_name LIKE 'B%';
 
 -- List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT 
+	e.emp_no,
+	e.first_name,
+	e.last_name
+	
+FROM employees e
+JOIN dept_emp de ON de.emp_no = e.emp_no
+JOIN departments d ON d.dept_no = de.dept_no
+	AND d.dept_name = 'Sales';
 
 -- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT 
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	d.dept_name
+	
+FROM employees e
+JOIN dept_emp de ON de.emp_no = e.emp_no
+JOIN departments d ON d.dept_no = de.dept_no
+	AND d.dept_name IN ('Sales', 'Development');
 
 -- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+SELECT 
+	e.last_name, 
+	COUNT(e.last_name) 
+	
+FROM employees e
+
+GROUP BY e.last_name
+ORDER BY COUNT(e.last_name) DESC;
